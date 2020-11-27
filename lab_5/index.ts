714d6371fa87422b7527e51a9b7296e75e20213a
sergiy-ta <serhiy819@gmail.com>
2020-11-27 23:11:57 +0200

    fixed lab 5

--- lab_5/index.ts
+++ lab_5/index.ts
@@ -1,42 +1,65 @@
-const p: number = 3;
-const q: number = 11;
+// It's message
+const M: bigint[] = [3n,1n,2n];
 
-const M: number[] = [3, 1, 2];
-const H: number[] = [6];
+// It's encrypt message
+const H: bigint[] = [6n];
 
-function findM1andM2(p: number, q: number, M: number[], H: number[]): { m1: number, m2: number } {
-    const n: number = p * q;
-    const w = (): number => (p - 1) * (q - 1);
-    const e = (): number => Math.floor(Math.random() * (w() - 1) + 1);
-    const e_number: number = e();
-    const d = () => {
-        function d1(): number {
-            let mod: number = 1 % w();
-            let d: number = 0;
-            while ((e_number * d) % w() !== mod) d++;
-            return d;
-        }
+// It's two mutually prime numbers
+const p: bigint = 2999n;
+const q: bigint = 4999n;
 
-        return d1() < n ? d1() : undefined;
+// It's product two mutually prime numbers
+const n: bigint = p * q;
+
+// It's functional Euler
+const w: bigint = (p - 1n) * (q - 1n);
+
+// It's open key
+const e: bigint = 4561n;
+
+// It's secret key
+const d: bigint = findD();
+
+// It's hash value
+const m1: bigint = findM1();
+
+// It's digital signature
+const S: bigint = m1 ** d % n;
+
+// It's hash value
+const m2: bigint = S ** e % n;
+
+// It's function for find secret key
+function findD(): bigint {
+    let number: bigint = 0n;
+    let mod: bigint = 1n % w;
+    while ((e * number) % w !== mod && number < n) {
+        number++;
     }
-
-    for (let i: number = 0; i < M.length; i++) {
-        H[i + 1] = (M[i] + H[i]) ** 2 % 33;
-    }
-
-    const m1: number = H[H.length - 1];
-
-    const S: number = m1 ** d() % n;
-    const m2: number = S ** e_number % n;
-
-    return { m1, m2 };
+    return number;
 }
 
-function analytics(m1, m2): void {
-    console.log(`Цифровий підпис: ${m1}`);
-    console.log(`Хеш значення: ${m2}`);
+// It's function for find hash value
+function findM1(): bigint {
+    for (let i: number = 0; i < M.length; i++) {
+        H[i + 1] = findH(M[i], H[i]);
+    }
+
+    return H[H.length - 1];
+}
+
+// It's function for find number H
+function findH(M: bigint, H: bigint): bigint {
+    return ((M + H) ** 2n) % n;
+}
+
+// It's function for algorithm analysis
+function analytics(): void {
+    console.log(`Хеш значення 1: ${m1}`);
+    console.log(`Цифровий підпис: ${S}`);
+    console.log(`Хеш значення 2: ${m2}`);
     console.log(`Успішно: ${m1 === m2}`);
 }
 
-let { m1, m2 } = findM1andM2(p, q, M, H);
-analytics(m1, m2);
+// Run the function
+analytics();
