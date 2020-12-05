e48f9d469d24dc74e14acbc65fb10ae58f0a3057
sergiy-ta <serhiy819@gmail.com>
2020-12-05 21:17:49 +0200

    fixed lab 6

--- lab_6/index.ts
+++ lab_6/index.ts
@@ -1,63 +1,126 @@
-// It's number p and q
-const p: number = 7;
-const q: number = 11;
+//It's function for find random number
+function getRandomIntNumber(max: number = 10000, min: number = 100000): number {
+    return Math.floor(Math.random() * (max - min) + min);
+}
 
-// It's number n
-const n: number = p * q;
+// It's function of verification if number is prime
+function isPrime(number: number): boolean {
+    if (number % 2 === 0 || number % 3 === 0 || number % 5 === 0 || number % 7 === 0 || number % 11 === 0) return false;
 
-// It's number yp and yq
-const yp: number = -3;
-const yq: number = 2;
+    for (let i: number = 13; i <= number / 2 + 1; i += 2) {
+        if (number % i === 0) return false;
+    }
 
-// It's message
-const m: number = 24;
-const wm: number = 17;
+    return true;
+}
 
-// It's cyphered message
-const C: number = encrypt(m);
+// It's function for get prime number
+function getPrimeNumber(): Promise<bigint> {
+    return new Promise<bigint>(resolve => {
+        let number: number;
 
-// It's decrypted messages
-const { r1, r2, r3, r4 } = decrypt(C);
-
-// It's correct message
-const correctR: number = findCorrectR(r1, r2, r3, r4);
+        do {
+            number = getRandomIntNumber();
+            if (number % 4 === 3 && isPrime(number)) resolve(BigInt(number));
+        } while (number % 4 !== 3 || !isPrime(number));
+    });
+}
 
 // It's function for encryption message
-function encrypt(m :number): number {
-    return m ** 2 % n;
+function encrypt(text: string, n: bigint): string {
+    let text_crypt: string = "";
+
+    for (let i = 0; i < text.length; i++) {
+        let m: bigint = BigInt(text.charCodeAt(i));
+
+        const C: bigint = (m ** 2n) % n;
+
+        text_crypt += String.fromCharCode(parseInt(C.toString()));
+    }
+
+    return text_crypt;
+}
+
+// It's function for find yp and yq
+function findYpYq(p: bigint, q: bigint): { yp: bigint, yq: bigint } {
+    let yp: bigint[] = [1n, 0n];
+    let yq: bigint[] = [0n, 1n];
+    let r: bigint[] = [p, q];
+    let t: bigint[] = [];
+
+    while (r[r.length - 1] !== 1n) {
+        t.push(r[r.length - 2] / r[r.length - 1]);
+        r.push(r[r.length - 2] % r[r.length - 1]);
+        yp.push(yp[yp.length - 2] - t[t.length - 1] * yp[yp.length - 1]);
+        yq.push(yq[yq.length - 2] - t[t.length - 1] * yq[yq.length - 1]);
+    }
+
+    return {
+        yp: yp[yp.length - 1],
+        yq: yq[yq.length - 1]
+    };
 }
 
 // It's function for decrypt message
-function decrypt(C: number): { r1: number, r2: number, r3: number, r4: number } {
-    // It's number mp and mq
-    let mp: number = C ** (0.25 * (p + 1)) % p;
-    let mq: number = C ** (0.25 * (q + 1)) % q;
+function decrypt(encrypt_text: string, p: bigint, q: bigint, n: bigint, yp: bigint, yq: bigint): string {
+    let text_decrypt: string = "";
+
+    for (let i = 0; i < encrypt_text.length; i++) {
+        let C: bigint = BigInt(encrypt_text.charCodeAt(i));
+
+        // It's number mp and mq
+        let mp: bigint = (C ** ((p + 1n) / 4n)) % p;
+        let mq: bigint = (C ** ((q + 1n) / 4n)) % q;
+
+        // It's number r1, r2, r3, r4
+        let r1: bigint = ((yp * p * mq) + (yq * q * mp)) % n;
+        let r2: bigint = n - r1;
+        let r3: bigint = ((yp * p * mq) - (yq * q * mp)) % n;
+        let r4: bigint = n - r3;
+
+        r1 = r1 < 0 ? r1 * -1n : r1;
+        r2 = r2 < 0 ? r2 * -1n : r2;
+        r3 = r3 < 0 ? r3 * -1n : r3;
+        r4 = r4 < 0 ? r4 * -1n : r4;
+
+        let r: number = Math.min(parseInt(r1.toString()), parseInt(r2.toString()), parseInt(r3.toString()), parseInt(r4.toString()));
+
+        text_decrypt += String.fromCharCode(parseInt(r.toString()));
+    }
+
+    return text_decrypt;
+}
+
+// It's function for algorithm analysis
+function analitics(text: string, encrypt_text: string, decrypt_text: string): void {
+    console.log(`Оригінальний текст: ${text}`);
+    console.log(`Зашифрований текст: ${encrypt_text}`);
+    console.log(`Дешифрований текст: ${decrypt_text}`);
+    console.log(`Тексти співпадають ${text === decrypt_text}`);
+}
+
+// It's main function
+async function main() {
+    // It's two prime numbers
+    const p: bigint = await getPrimeNumber();
+    const q: bigint = await getPrimeNumber();
+
+    // It's open key
+    const n: bigint = p * q;
+
+    // It's number yp and yq
+    const { yp, yq } = findYpYq(p, q);
+
+    // It's message
+    const text: string = "Text";
+
+    // It's cyphered message
+    const encrypt_text: string = encrypt(text, n);
     
-    // It's number r1, r2, r3, r4
-    let r1: number = ((yp * p * mq + yq * q * mp) % n) + n;
-    let r2: number = n - r1;
-    let r3: number = ((yp * p * mq - yq * q * mp) % n) + n;
-    let r4: number = n - r3;
+    // It's decrypted messages
+    const decrypt_text: string = decrypt(encrypt_text, p, q, n, yp, yq);
 
-    return { r1, r2, r3, r4 };
+    analitics(text, encrypt_text, decrypt_text);
 }
 
-function findCorrectR(r1: number, r2: number, r3: number, r4: number): number {
-    const number: number = 7;
-    if (r1 % wm === number) return r1;
-    else if (r2 % wm === number) return r2;
-    else if (r3 % wm === number) return r3;
-    else if (r4 % wm === number) return r4;
-    return NaN;
-}
-
-function analytics(): void {
-    console.log(`Оригінальний текст: ${m}`);
-    console.log(`Зашифрований текст: ${C}`);
-    console.log(`Дешифровані тексти: ${r1}, ${r2}, ${r3}, ${r4}`);
-    console.log(`Успішно: ${r1 === m || r2 === m || r3 === m || r4 === m}`);
-    console.log(`Дешифрований текст: ${correctR}`);
-    console.log(`Успішно: ${m === correctR}`);
-}
-
-analytics();
+main();
