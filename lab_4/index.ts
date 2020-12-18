dddab28547c9146a42972a5ef69ebb3ac35a1daf
sergiy-ta <serhiy819@gmail.com>
2020-12-18 16:46:36 +0200

    fixed lab 4

--- lab_4/index.ts
+++ lab_4/index.ts
@@ -1,30 +1,59 @@
-// It's message
-const M: bigint[] = [3n,1n,2n];
+// //It's function for find random number
+function getRandomIntNumber(max: number = 100000, min: number = 10000): number {
+    return Math.floor(Math.random() * (max - min) + min);
+}
 
-// It's encrypt message
-const C: bigint[] = [];
+// It's function for get prime number
+function getTwoPrimeNumber(): Promise<{p: bigint, q: bigint}> {
+    return new Promise<{p: bigint, q: bigint}>(resolve => {
+        let p: bigint;
+        let q: bigint;
 
-// It's decrypt message
-const new_M: bigint[] = [];
+        do {
+            p = BigInt(getRandomIntNumber());
+            q = BigInt(getRandomIntNumber());
+            if (isTwoPrime(p, q)) resolve({p, q});
+        } while (!isTwoPrime(p, q));
+    });
+}
 
-// It's two mutually prime numbers
-const p: bigint = 2999n;
-const q: bigint = 4999n;
+// It's function for verificate two number in GCD = 1
+function isTwoPrime(number1: bigint, number2: bigint): boolean | void {
+    let number1_list: bigint[] = [];
+    let number2_list: bigint[] = [];
 
-// It's product two mutually prime numbers
-const n: bigint = p * q;
+    for (let i: number = 2; i <= number1 / 2n; i++) {
+        if (number1 % BigInt(i) === 0n) number1_list.push(BigInt(i));
+    }
 
-// It's functional Euler
-const w: bigint = (p - 1n) * (q - 1n);
+    for (let i: number = 2; i <= number2 / 2n; i++) {
+        if (number2 % BigInt(i) === 0n) number2_list.push(BigInt(i));
+    }
 
-// It's open key
-const e: bigint = 4561n;
+    if (number1_list.length === 0 || number2_list.length === 0) return true;
+    
+    for (let index: number = 0; index < number1_list.length; index++) {
+        if (number2_list.includes(number1_list[index])) {
+            return false;
+        }
 
-// It's secret key
-const d: bigint = findD();
+        if (index === number1_list.length - 1) return true;
+    }
+}
+
+function findE(w: bigint): Promise<bigint> {
+    return new Promise<bigint>(resolve => {
+        let e: bigint;
+
+        do {
+            e = BigInt(getRandomIntNumber(parseInt((w - 1n).toString()), 2));
+            if (isTwoPrime(e, w)) resolve(e);
+        } while (!isTwoPrime(e, w));
+    });
+}
 
 // It's function for find number D
-function findD(): bigint {
+function findD(w: bigint, n: bigint, e: bigint): bigint {
     let number: bigint = 0n;
     let mod: bigint = 1n % w;
     while ((e * number) % w !== mod && number < n) {
@@ -34,39 +63,81 @@ function findD(): bigint {
 }
 
 // It's function for find number C
-function findC(M: bigint): bigint {
+function findC(M: bigint, e: bigint, n: bigint): bigint {
     return (M ** e) % n;
 }
 
 // It's function for find number decrypted M
-function findM(C: bigint): bigint {
+function findM(C: bigint, d: bigint, n: bigint): bigint {
     return (C ** d) % n;
 }
 
-// It's function for encrypt message
-function encrypted() {
-    M.forEach(M => {
-        C.push(findC(M));
-    });
+// It's function for encryption message
+function encrypt(text: string, e: bigint, n: bigint): string {
+    let text_crypt: string = "";
+
+    for (let i = 0; i < text.length; i++) {
+        let m: bigint = BigInt(text.charCodeAt(i));
+
+        let C: bigint = findC(m, e, n);
+
+        text_crypt += String.fromCharCode(parseInt(C.toString()));
+    }
+
+    return text_crypt;
 }
 
 // It's function for decrypt message
-function decrypted() {
-    C.forEach(C => {
-        new_M.push(findM(C));
-    });
+function decrypt(encrypt_text: string, d: bigint, n: bigint): string {
+    let text_decrypt: string = "";
+
+    for (let i = 0; i < encrypt_text.length; i++) {
+        let C: bigint = BigInt(encrypt_text.charCodeAt(i));
+
+        let M: bigint = findM(C, d, n);
+
+        text_decrypt += String.fromCharCode(parseInt(M.toString()));
+    }
+
+    return text_decrypt;
 }
 
 // It's function for algorithm analysis
-function analytics(): void {
-    console.log(`Оригінальний масив: ${M}`);
-    console.log(`Зашифрований масив: ${C}`);
-    console.log(`Дешифрований масив: ${new_M}`);
-    console.log(`Успішно: ${M.join() === new_M.join()}`);
+function analitics(text: string, encrypt_text: string, decrypt_text: string): void {
+    console.log(`Оригінальний текст: ${text}`);
+    console.log(`Зашифрований текст: ${encrypt_text}`);
+    console.log(`Дешифрований текст: ${decrypt_text}`);
+    console.log(`Тексти співпадають ${text === decrypt_text}`);
 }
 
 
-// Run the functions
-encrypted();
-decrypted();
-analytics();
+async function run() {
+    // It's two mutually prime numbers
+    const { p, q } = await getTwoPrimeNumber();
+
+    // It's product two mutually prime numbers
+    const n: bigint = p * q;
+
+    // It's functional Euler
+    const w: bigint = (p - 1n) * (q - 1n);
+
+    // It's open key
+    const e: bigint = await findE(w);
+
+    // It's secret key
+    const d: bigint = findD(w, n, e);
+    
+    // It's message
+    const text: string = "Text";
+
+    // It's cyphered message
+    const encrypt_text: string = encrypt(text, e, n);
+    
+    // It's decrypted messages
+    const decrypt_text: string = decrypt(encrypt_text, d, n);
+
+    // Run by function for analytics
+    analitics(text, encrypt_text, decrypt_text);
+}
+
+run();
